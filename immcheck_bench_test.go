4325e7af6c2bc8ccd7745dd86ce87c4a74df571e
bstorozhuk <storozhuk.b.m@gmail.com>
2021-08-14 20:27:01 +0100

    Deep structure check benchmark

--- immcheck_bench_test.go
+++ immcheck_bench_test.go
@@ -30,19 +30,27 @@ var settings = []immcheck.ImutabilityCheckOptions{
 	},
 }
 
-var sizeOfSlice = []int{
-	8, 16, 32, 64, 4 * 1024, 16 * 1024,
+var sizeOfByteSlice = []int{
+	16 * 1024,
 }
 
 var percentOfMutations = []int{
-	0, // 1, 99,
+	0, 1, // 99,
+}
+
+var countOfTransactions = []int{
+	8,
+}
+
+var sizeOfTxContext = []int{
+	1024,
 }
 
 var count = 0
 
 func BenchmarkImmcheckBytes(b *testing.B) {
 	for _, options := range settings {
-		for _, targetSize := range sizeOfSlice {
+		for _, targetSize := range sizeOfByteSlice {
 			for _, mutationPercent := range percentOfMutations {
 				benchName := fmt.Sprintf("[%v]byte;muts(%v%%)", targetSize, mutationPercent)
 				if options.SkipStringSnapshotCapturing {
@@ -79,5 +87,193 @@ func BenchmarkImmcheckBytes(b *testing.B) {
 				})
 			}
 		}
+	}
+}
+
+func BenchmarkImmcheckTransactions(b *testing.B) {
+	for _, options := range settings {
+		for _, txCnt := range countOfTransactions {
+			for _, ctxSize := range sizeOfTxContext {
+				for _, mutationPercent := range percentOfMutations {
+					benchName := fmt.Sprintf("[%v]txs(%v);muts(%v%%)", txCnt, ctxSize, mutationPercent)
+					if options.SkipStringSnapshotCapturing {
+						benchName += ";NoSnap"
+					}
+					if options.SkipOriginCapturing {
+						benchName += ";NoOrig"
+					}
+					b.Run(benchName, func(b *testing.B) {
+						localRand := rand.New(rand.NewSource(rand.Int63()))
+						count = 0
+
+						targetObjects := make([][]*Transaction, b.N)
+						for i := 0; i < b.N; i++ {
+							targetObjects[i] = make([]*Transaction, txCnt)
+							for j := 0; j < txCnt; j++ {
+								targetObjects[i][j] = GenerateTransaction(localRand, ctxSize)
+							}
+						}
+
+						b.ResetTimer()
+						b.ReportAllocs()
+						for i := 0; i < b.N; i++ {
+							snapshot := immcheck.NewValueSnapshotWithOptions(&targetObjects[i], options)
+							rndValue := rand.Intn(100)
+							if rndValue < mutationPercent {
+								targetObjects[i][0].Amount.Amount = Amount(rndValue)
+							}
+							otherSnapshot := immcheck.NewValueSnapshotWithOptions(&targetObjects[i], options)
+							err := snapshot.CheckImmutabilityAgainst(otherSnapshot)
+							if err != nil {
+								count += 1
+							}
+						}
+						b.ReportMetric(float64(count), "muts")
+					})
+				}
+			}
+		}
+	}
+}
+
+type CurrencyCode int
+
+const (
+	USD CurrencyCode = iota
+	EUR
+)
+
+type Amount int64
+
+type Currency struct {
+	Code     CurrencyCode
+	Fraction uint64
+}
+
+var Currencies = map[CurrencyCode]Currency{
+	USD: {Code: USD, Fraction: 2},
+	EUR: {Code: EUR, Fraction: 2},
+}
+
+type Money struct {
+	Currency Currency
+	Amount   Amount
+}
+
+type AccountType int
+
+const (
+	Credit AccountType = iota
+	Debit
+)
+
+type Account struct {
+	Address [16]byte
+	Type    AccountType
+}
+
+type AccountState struct {
+	Account Account
+	Balance Money
+}
+
+type StateSnapshot struct {
+	SrcState AccountState
+	DstState AccountState
+}
+
+type Transaction struct {
+	Src         Account
+	Dst         Account
+	Amount      Money
+	StateBefore *StateSnapshot
+	StateAfter  *StateSnapshot
+	TxContext   map[string]string
+	Attachments map[string]interface{}
+}
+
+func GenerateTransaction(rnd *rand.Rand, contextSize int) *Transaction {
+	currencyCode := CurrencyCode(rnd.Intn(2))
+	targetCurrency := Currencies[currencyCode]
+
+	srcAddress := [16]byte{}
+	rnd.Read(srcAddress[:])
+	srcAccount := Account{
+		Address: srcAddress,
+		Type:    AccountType(rnd.Intn(2)),
+	}
+
+	dstAddress := [16]byte{}
+	rnd.Read(dstAddress[:])
+	dstAccount := Account{
+		Address: dstAddress,
+		Type:    AccountType(rnd.Intn(2)),
+	}
+
+	transferAmount := Money{
+		Currency: targetCurrency,
+		Amount:   Amount(int64(rnd.Uint32()) + 1),
+	}
+
+	before := &StateSnapshot{
+		SrcState: AccountState{
+			Account: srcAccount,
+			Balance: Money{
+				Currency: targetCurrency,
+				Amount:   Amount(int64(transferAmount.Amount) + int64(rnd.Uint32())),
+			},
+		},
+		DstState: AccountState{
+			Account: dstAccount,
+			Balance: Money{
+				Currency: targetCurrency,
+				Amount:   Amount(int64(rnd.Uint32())),
+			},
+		},
+	}
+
+	after := &StateSnapshot{
+		SrcState: AccountState{
+			Account: srcAccount,
+			Balance: Money{
+				Currency: targetCurrency,
+				Amount:   Amount(int64(before.SrcState.Balance.Amount) - int64(transferAmount.Amount)),
+			},
+		},
+		DstState: AccountState{
+			Account: dstAccount,
+			Balance: Money{
+				Currency: targetCurrency,
+				Amount:   Amount(int64(before.DstState.Balance.Amount) + int64(transferAmount.Amount)),
+			},
+		},
+	}
+
+	txContext := make(map[string]string, contextSize)
+	for i := 0; i < contextSize; i++ {
+		key := make([]byte, rand.Intn(128))
+		value := make([]byte, rand.Intn(4096))
+		rnd.Read(key)
+		rnd.Read(value)
+		txContext[string(key)] = string(value)
+	}
+
+	return &Transaction{
+		Src:         srcAccount,
+		Dst:         dstAccount,
+		Amount:      transferAmount,
+		StateBefore: before,
+		StateAfter:  after,
+		TxContext:   txContext,
+		Attachments: map[string]interface{}{
+			"bank": struct {
+				Name        string
+				Reliability uint
+			}{
+				"TestBank",
+				1,
+			},
+			"certificate": []byte{1, 2, 3},
+		},
 	}
 }
